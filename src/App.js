461d7054a263e01fa97040f50795b3cbae8cf7f4
Sekou S. Seh <sehsekou98@gmail.comh>
2023-02-10 18:34:46 +0100

    notes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Note from './components/Note';
+import notes from './components/notes'
+
+
 
 function App() {
   return (
     <div>
       <Header />
+      {notes.map(noteItem => (
+         <Note
+         key={noteItem.key}
+         title={noteItem.title}
+         content={noteItem.content}
+          />
+      ))}
       <Footer />
-      <Note />
     </div>
   );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 
-const Note = () => {
+function Note(props)  {
   return (
     <div className='note'>
-        <h1>This is the title</h1>
-        <p>This is the content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
-  )
+  );
 }
 
-export default Note
+export default Note;
